39da1120b1190954e9e308a1e7561c83ce8793cd
Testgit <seyodwitthawat@hotmail.com>
2018-01-11 08:45:56 +0700

    first commit

---
+++ src/main/webapp/scripts/app/contact.js
@@ -0,0 +1,37 @@
+
+
+
+function initializeMap() {
+
+    var lat = '13.968177'; //Set your latitude.
+    var lon = '100.599283'; //Set your longitude.
+
+    var centerLon = lon - 0.0105;
+
+    var myOptions = {
+        // scrollwheel: false,
+        // draggable: false,
+        //disableDefaultUI: true,
+        center: new google.maps.LatLng(lat, centerLon),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    //Bind map to elemet with id map-canvas
+    var map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
+    var marker = new google.maps.Marker({
+        map: map,
+        position: new google.maps.LatLng(lat, lon),
+
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: "Your content goes here!"
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+    });
+
+    infowindow.open(map, marker);
+}
