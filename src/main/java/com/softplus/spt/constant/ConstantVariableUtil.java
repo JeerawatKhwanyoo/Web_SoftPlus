3f1ee69ca54d0bf31076a670a194a2dd5ae826a0
JeerawatKhwanyoo <34857012+JeerawatKhwanyoo@users.noreply.github.com>
2018-01-24 08:45:25 +0700

    create constant

---
+++ src/main/java/com/softplus/spt/constant/ConstantVariableUtil.java
@@ -0,0 +1,22 @@
+package com.softplus.spt.constant;
+
+public class ConstantVariableUtil {
+
+    public static final String MAIL_SMTP_HOST= "smtp.gmail.com";
+    public static final String MAIL_SMTP_SOCKETFACTORY_PORT="465";
+    public static final String MAIL_SMTP_SOCKETFACTORY_CLASS="javax.net.ssl.SSLSocketFactory";
+    public static final String MAIL_SMTP_AUTH="true";
+    public static final String MAIL_SMTP_PORT="465";
+
+    public static final String SENDER_MAIL_USERNAME="inoncpe@gmail.com";
+    public static final String SENDER_MAIL_PASSWORD="inon2537";
+    public static final String RECIEVER_MAIL="yodwitthawat@hotmail.com,khwanyoo.ns@gmail.com";
+    public static final String CC_MAIL="aphisiit086757@hotmail.com";
+
+    public static final String TITLE_MAIL="ติดต่อสมัครสหกิจศึกษา(Cooperation)";
+//    siriradc64@gmail.com
+
+
+
+
+}
